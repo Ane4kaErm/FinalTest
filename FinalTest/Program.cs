8e6c577a421dc354e471e1827baa5d8a19069504
AnnPCMaibenben <ann.erma4kova@gmail.com>
2022-12-11 23:46:09 +0300

    check array method ready

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[4] {"Hello", "2", "world", ":-)"};
+string[] arrayNew = new string[array.Length];
+
+void CheckArray(string[] array, string[] arrayNew)
+{
+    int result = 0;
+    int length = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) 
+        {
+            arrayNew[result] = array[i];
+            result++;
+        }
+    }
+}
